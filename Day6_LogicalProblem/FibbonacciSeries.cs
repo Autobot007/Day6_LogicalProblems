834afca88a1c29c30491e442d24e18a283d0f0d0
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-08 21:05:54 +0530

    [SARFARAAZ] Q1 fibbonacci Series added

---
+++ Day6_LogicalProblem/FibbonacciSeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_LogicalProblem
+{
+    internal class FibbonacciSeries
+    {
+        int firstNumber = 0;
+        int secondNumber = 1;
+        int result;
+
+        public void PrintFibbonacci()
+        {
+            Console.WriteLine("Enter number to fibbonacci series");
+            int number = Convert.ToInt16(Console.ReadLine());
+            for (int i = 0; i <= number; i++)
+            {
+                Console.WriteLine("{0}", firstNumber);
+                result = firstNumber + secondNumber;
+                firstNumber = secondNumber;
+                secondNumber = result;
+            }
+        }
+    
+    }
+}
+
+
