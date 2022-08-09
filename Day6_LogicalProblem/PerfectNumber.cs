fc7125482bedf64a9e6ad2b074bc1535eec5f975
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-09 13:38:09 +0530

    [SARFARAAZ]-Q2 Perfect Number added

---
+++ Day6_LogicalProblem/PerfectNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_LogicalProblem
+{
+    internal class PerfectNumber
+    {
+
+        public static void Q2Perfectnumber()
+        {
+            int num = Convert.ToInt16(Console.ReadLine());
+            int sum = 1;
+
+            for (int i = 2; i * i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    if (i * i != num)
+                        sum = sum + i + num / i;
+                    else
+                        sum = sum + i;
+                }
+
+            }
+            if (sum == num && num != 1)
+                Console.WriteLine("{0} is the Perfect number", num);
+            else
+                Console.WriteLine("{0} is not a Perfect number", num);
+
+
+
+        }
+    }
+}
+ 
--- Day6_LogicalProblem/Program.cs
+++ Day6_LogicalProblem/Program.cs
@@ -6,8 +6,35 @@ namespace Day6_LogicalProblems
     {
         public static void Main(string[] args)
         {
+
+            Console.WriteLine("++++++++++Welcome to Day6 Problems++++++++++");
+            Console.WriteLine("============================================");
+           
+            while (true)
+            {
+                Console.WriteLine("Enter \n 1 for Fibbonacci series \n 2 for Perfect Number");
+                int enter = Convert.ToInt32(Console.ReadLine());
+                switch (enter)
+                {
+                    case 1:
+                        FibbonacciSeries obj = new FibbonacciSeries();
+                        obj.PrintFibbonacci();
+                        Console.WriteLine("");
+                        break;
+                    case 2:
+                        PerfectNumber.Q2Perfectnumber();
+                        Console.WriteLine("");
+                        break;
+                    default:
+                        Console.WriteLine("");
+                        break;
+                }
+
+            }
+
             FibbonacciSeries problem1 = new FibbonacciSeries();
             problem1.PrintFibbonacci();
         }
     }
-}
+} 
+
