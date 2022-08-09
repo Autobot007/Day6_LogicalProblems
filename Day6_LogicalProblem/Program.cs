256c91a5d6869beee60a5c2482fe4db9667264d9
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-09 12:36:24 +0530

    [SARFARAAZ] Q1 Fibbonacci Series Added

--- Day6_LogicalProblem/Program.cs
+++ Day6_LogicalProblem/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Day6_LogicalProblems
+﻿using Day6_LogicalProblem;
+
+namespace Day6_LogicalProblems
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-
+            FibbonacciSeries problem1 = new FibbonacciSeries();
+            problem1.PrintFibbonacci();
         }
     }
 }
