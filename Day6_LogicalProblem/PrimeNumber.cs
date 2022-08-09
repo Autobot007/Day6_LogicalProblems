63c3af2b478de7d750470482f5e96679f986a50e
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-09 15:16:47 +0530

    [SARFARAAZ]-Q3 Prime Number Added

---
+++ Day6_LogicalProblem/PrimeNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Day6_LogicalProblem
+{
+    public class PrimeNumber
+    {
+        public static void Q2PrimeNumber()
+        {
+            Console.WriteLine("Please Enter Number");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int temp = 0;
+
+            for(int i = 2; i <= num-1; i++)
+            {
+                if (num % i == 0)
+                   temp++;
+                
+            }
+            if (temp == 0)
+                Console.WriteLine("{0} is a Prime Number", num);
+            else 
+                Console.WriteLine("{0} is not a Prime Number",num);
+            Console.WriteLine("");
+
+
+        }
+    }
+}
+
+
--- Day6_LogicalProblem/Program.cs
+++ Day6_LogicalProblem/Program.cs
@@ -12,7 +12,7 @@ namespace Day6_LogicalProblems
            
             while (true)
             {
-                Console.WriteLine("Enter \n 1 for Fibbonacci series \n 2 for Perfect Number");
+                Console.WriteLine("Enter \n 1 for Fibbonacci series \n 2 for Perfect Number \n 3 for Prime Number");
                 int enter = Convert.ToInt32(Console.ReadLine());
                 switch (enter)
                 {
@@ -23,6 +23,10 @@ namespace Day6_LogicalProblems
                         break;
                     case 2:
                         PerfectNumber.Q2Perfectnumber();
+                        Console.WriteLine("");
+                        break;
+                    case 3:
+                        PrimeNumber.Q2PrimeNumber();
                         Console.WriteLine("");
                         break;
                     default:
